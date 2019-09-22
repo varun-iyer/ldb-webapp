c498d7c811eda3f09f4055a951c18c1e814d1a52
Varun Iyer <varun_iyer@posteo.net>
2019-09-22 12:28:25 -0700

    Updated flask shell variables

--- ldb.py
+++ ldb.py
@@ -1,6 +1,6 @@
 from app import app, db
 from app.models import Document, reference
-from app.ref import get_pub, build_graph
+from app.ref import get_doc, build_graph
 from networkx.drawing.nx_pydot import write_dot
 
 @app.shell_context_processor
@@ -9,7 +9,7 @@ def make_shell_context():
             'db': db,
             'Document': Document,
             'reference':reference,
-            'get_pub':get_pub,
+            'get_doc':get_doc,
             'build_graph':build_graph,
             'write_dot':write_dot
         }
