e17f168551a9d608c4ede7a83e7fe9d92f1f422e
Varun Iyer <varun_iyer@posteo.net>
2019-10-15 12:00:29 -0700

    Working home + dummy login

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
+from flask import render_template
 from app import app
+from app.forms import LoginForm
 
-@app.route("/")
-@app.route("/index")
+@app.route('/')
+@app.route('/index')
 def index():
-    return "Hello World!"
+    return render_template('index.html')
+ 
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+class Config(object):
+        SECRET_KEY = os.environ.get("SECRET_KEY") or "Ko89OD9PjEF+FrX5AvjlrnWHrqXjjG2IXaqY2rxo/yz0Vf7o/Dx8Mg=="
