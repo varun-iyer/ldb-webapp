7cee84bd5922434c92563b648de786138391f89a
Varun Iyer <varun_iyer@posteo.net>
2019-10-16 13:45:15 -0700

    Added Collection and Documents

--- app/models.py
+++ app/models.py
@@ -1,13 +1,19 @@
 from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+
+user_collection = db.Table('user_collection',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('collection_id', db.Integer, db.ForeignKey('collection.id'))
+)
  
 class User(UserMixin, db.Model):
-    # we want to use discord-style name/id combos so index ids as well as names
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    collections = db.relationship('Collection', secondary=user_collection)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -18,6 +24,22 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+class Collection(db.Model):
+    __tablename__ = 'collection'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    documents = db.relationship('Document', backref='collection', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Collection {}>'.format(self.name)
+
+
+class Document(db.Model):
+    # TODO how to correlate documents that are the same in diff. collections?
+    id = db.Column(db.Integer, primary_key=True)
+    collection_id = db.Column(db.Integer, db.ForeignKey('collection.id'))
+
+ 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
