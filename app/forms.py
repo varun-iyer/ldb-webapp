af6a7c6d10d61ea038462de10a39ebe031eb4928
Varun Iyer <varun_iyer@posteo.net>
2019-10-26 12:45:39 -0700

    only graph form

--- app/forms.py
+++ app/forms.py
@@ -1,31 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from app.models import User
 
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
-
-class SignupForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
-    name = StringField('Display Name')
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Sign Up')
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is not None:
-            raise ValidationError('An account already exists for this email address')
- 
-class CollectionForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    description = StringField('Description')
-    submit = SubmitField('Create')
-
- 
-class DocumentForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    submit = SubmitField('Create')
+class GraphForm(FlaskForm):
+    name = StringField('DOI', validators=[DataRequired()])
+    submit = SubmitField('Build Graph')
