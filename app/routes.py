3a9ece46e00c182a06210bdcec5238626526dd02
Varun Iyer <varun_iyer@posteo.net>
2019-10-30 20:46:02 -0700

    entering DOIs

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 import flask
+from urllib.parse import quote, unquote
 from app.models import Document
+from app.ref import build_graph, graph_svg
 from app import app, db
 from app.forms import GraphForm
 
@@ -8,5 +10,11 @@ from app.forms import GraphForm
 def index():
     form = GraphForm()
     if form.validate_on_submit():
-        return flask.render_template('index.html', form=form)
+        return flask.redirect(flask.url_for('results', doi=quote(form.doi.data, safe='')))
     return flask.render_template('index.html', form=form)
+
+@app.route('/results/<doi>', methods=['GET'])
+def results(doi):
+    g = build_graph(unquote(doi))
+    s = graph_svg(g)
+    return flask.render_template('results.html', graphsvg=s)
